686cdd200debd1144524e33769018074655d8c05
baranovdv <baranov.d.v.1988@gmail.com>
2024-07-08 01:57:34 +0500

    feat: add catalogBatchProcess test

---
+++ product-service/lambda/catalogBatchProcess.test.ts
@@ -0,0 +1,50 @@
+import { SNSClient } from "@aws-sdk/client-sns";
+import { handler } from "./catalogBatchProcess";
+import { SQSEvent } from "aws-lambda";
+
+jest.mock("@aws-sdk/client-sns", () => {
+  return {
+    SNSClient: jest.fn().mockReturnValue({
+      send: jest.fn(),
+    }),
+    PublishCommand: jest.fn(),
+  };
+});
+
+describe("handler", () => {
+  let snsClientMock: any;
+
+  beforeEach(() => {
+    snsClientMock = new SNSClient();
+    jest.clearAllMocks();
+  });
+
+  it("Should process items and send SNS calls", async () => {
+    const event = {
+      Records: [
+        {
+          body: JSON.stringify([
+            {
+              id: "0",
+              title: "Product 1",
+              description: "Description 1",
+              price: 1,
+              count: 2,
+            },
+            {
+              id: "1",
+              title: "Product 2",
+              description: "Description 2",
+              price: 10,
+              count: 20,
+            },
+          ]),
+        },
+      ],
+    };
+
+    await handler(event as SQSEvent);
+
+    expect(snsClientMock.send).toHaveBeenCalledTimes(2);
+  });
+});
