b0ec2f7a8bd9fac5939205c4456c4ae68f3ac3f4
baranovdv <baranov.d.v.1988@gmail.com>
2024-08-11 23:26:34 +0500

    fix: minor

--- bff-service/app/src/index.js
+++ bff-service/app/src/index.js
@@ -5,7 +5,7 @@ import { config } from "dotenv";
 config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.all("/*", async (req, res) => {
   try {
